dc865ebb5c294379e12c30e527d9ea80885115a2
Gustavo Gutierrez <gustavogutierrezgt@gmail.com>
2025-01-18 15:12:40 +1000

    adionando o exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -6,8 +6,17 @@
 
 
 #10. Escreva um programa que calcule a área do circulo, recebendo o raio como entrada.
-import math
-raio_circulo = float(input("Digite o raio do circulo: "))
-area_do_circulo = math.pi*raio_circulo**2
 
-print(f"{area_do_circulo:.2f}")
+# import math
+# raio_circulo = float(input("Digite o raio do circulo: "))
+# area_do_circulo = math.pi*raio_circulo**2
+# print(f"{area_do_circulo:.2f}")
+
+# 14. Faça um programa que peça para o usuario para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+# input("insira uma data no formato dd/mm/aaaa: ")
+data_do_usuario = "20/02/2024"
+data_separada = data_do_usuario.split("/")
+dia = print(f"dia: {data_separada[0]}")
+mes = dia = print(f"mes: {data_separada[1]}")
+ano = dia = print(f"ano: {data_separada[2]}")
