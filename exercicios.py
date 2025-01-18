74898e3cebfe6d338b8c519570637b695949210e
Gustavo Gutierrez <gustavogutierrezgt@gmail.com>
2025-01-18 13:48:47 +1000

    adicionando exercicio 04

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero_01 = int(input("Digite o primeiro número: "))
+numero_02 = int(input("Digite o segundo número: "))
+
+resultado = print(numero_01//numero_02)
